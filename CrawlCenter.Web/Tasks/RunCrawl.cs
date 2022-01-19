c5416d2e1bc2d6442eff177209d76691b4d524f0
DealiAxy <dealiaxy@gmail.com>
2022-01-19 23:00:27 +0800

    增加了两个todo

--- CrawlCenter.Web/Tasks/RunCrawl.cs
+++ CrawlCenter.Web/Tasks/RunCrawl.cs
@@ -2,6 +2,8 @@
 using CrawlCenter.Data.Models;
 
 namespace CrawlCenter.Web.Tasks {
+    // todo CrawlTask应该在构造函数里注入
+    // todo 构造函数需要传入环境变量、初始化配置等参数
     public class RunCrawl {
         public void Run(CrawlTask task) {
             var procInfo = new ProcessStartInfo {
