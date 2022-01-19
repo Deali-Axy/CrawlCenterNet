7fb902a02fc1ffeb8350548aceee66dcdb3bd999
DealiAxy <dealiaxy@gmail.com>
2022-01-19 20:59:01 +0800

    配置中心重构的过程中……

--- CrawlCenter.Shared/Models/MongodbSettings.cs
+++ CrawlCenter.Shared/Models/MongodbSettings.cs
@@ -4,4 +4,12 @@ public class MongodbSettings {
     public string ConfigCollectionName { get; set; }
     public string ConnectionString { get; set; }
     public string DatabaseName { get; set; }
+
+    public Collections Collections { get; set; }
+}
+
+public class Collections {
+    public string CommonConfig { get; set; }
+    public string ProjectConfig { get; set; }
+    public string CrawlTaskConfig { get; set; }
 }
---
+++ CrawlCenter.Web/Apis/ConfigCenter/CrawlConfigController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrawlCenter.Web.Apis.ConfigCenter; 
+
+/// <summary>
+/// 爬虫私有配置
+/// </summary>
+[ApiController]
+[Route("Api/Config/Crawl")]
+public class CrawlConfigController : ControllerBase {
+    
+}
