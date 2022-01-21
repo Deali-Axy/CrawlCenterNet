d578d8f23a22bb5571527dcaf240bfe58ea2bb37
DealiAxy <dealiaxy@gmail.com>
2022-01-21 17:34:13 +0800

    加了个控制器

---
+++ CrawlCenter.Web/Apis/CrawlController.cs
@@ -0,0 +1,20 @@
+﻿using CrawlCenter.Data.Models;
+using CrawlCenter.Data.Repositories;
+using CrawlCenter.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrawlCenter.Web.Apis; 
+
+[Authorize]
+[ApiController]
+[Route("Api/[controller]")]
+public class CrawlController : ControllerBase {
+    private readonly IAppRepository<CrawlTask> _crawlRepo;
+    private readonly AuthService _authService;
+
+    public CrawlController(IAppRepository<CrawlTask> crawlRepo, AuthService authService) {
+        _crawlRepo = crawlRepo;
+        _authService = authService;
+    }
+}
