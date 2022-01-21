48a4e4ac03543f604fc5481e24b3b3acb0fcc50e
DealiAxy <dealiaxy@gmail.com>
2022-01-21 17:34:35 +0800

    增加一个数据模型

--- CrawlCenter.Data/Extensions/ConfigureRepositories.cs
+++ CrawlCenter.Data/Extensions/ConfigureRepositories.cs
@@ -3,17 +3,18 @@ using CrawlCenter.Data.Repositories.Impl;
 using CrawlCenter.Data.Models;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CrawlCenter.Data.Extensions {
-    public static class ConfigureRepositories {
-        public static void AddRepositories(this IServiceCollection services) {
-            services.AddScoped<CommonConfigRepo>();
-            services.AddScoped<CrawlConfigRepo>();
-            services.AddScoped<ProjectConfigRepo>();
-            services.AddScoped<IAppRepository<CrawlTask>, CrawlTaskRepo>();
-            services.AddScoped<IAppRepository<Project>, ProjectRepo>();
-            services.AddScoped<IAppRepository<ProjectTag>, BaseSqlRepo<ProjectTag>>();
-            services.AddScoped<IAppRepository<RecurringTask>, RecurringTaskRepo>();
-            services.AddScoped<IAppRepository<User>, BaseSqlRepo<User>>();
-        }
+namespace CrawlCenter.Data.Extensions; 
+
+public static class ConfigureRepositories {
+    public static void AddRepositories(this IServiceCollection services) {
+        services.AddScoped<CommonConfigRepo>();
+        services.AddScoped<CrawlConfigRepo>();
+        services.AddScoped<ProjectConfigRepo>();
+        services.AddScoped<IAppRepository<CrawlTask>, CrawlTaskRepo>();
+        services.AddScoped<IAppRepository<Project>, ProjectRepo>();
+        services.AddScoped<IAppRepository<ProjectTag>, BaseSqlRepo<ProjectTag>>();
+        services.AddScoped<IAppRepository<RecurringTask>, RecurringTaskRepo>();
+        services.AddScoped<IAppRepository<User>, BaseSqlRepo<User>>();
+        services.AddScoped<IAppRepository<UserCrawlTask>, BaseSqlRepo<UserCrawlTask>>();
     }
 }
