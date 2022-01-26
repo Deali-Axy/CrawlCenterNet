849150ad6b86e90ff29136ff4c78ec39f85606f0
DealiAxy <dealiaxy@gmail.com>
2022-01-26 16:12:27 +0800

    修改了一下swagger配置

--- CrawlCenter.Web/Extensions/ConfigureSwagger.cs
+++ CrawlCenter.Web/Extensions/ConfigureSwagger.cs
@@ -3,24 +3,24 @@ using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
-namespace CrawlCenter.Web.Extensions {
-    public static class ConfigureSwagger {
-        public static void AddSwagger(this IServiceCollection services) {
-            services.AddSwaggerGen(options => {
-                options.SwaggerDoc("v1", new OpenApiInfo {Title = "CrawlCenter.Web API V1", Version = "v1"});
-                options.AddServer(new OpenApiServer {
-                    Url = "",
-                    Description = "CrawlCenter Master Server"
-                });
-                options.CustomOperationIds(apiDesc => {
-                    var controllerAction = apiDesc.ActionDescriptor as ControllerActionDescriptor;
-                    return $"{controllerAction?.ControllerName}-{controllerAction?.ActionName}";
-                });
+namespace CrawlCenter.Web.Extensions; 
 
-                // 为 Swagger JSON and UI设置xml文档注释路径
-                var xmlPath = Path.Combine(System.AppContext.BaseDirectory, "CrawlCenter.Web.xml");
-                options.IncludeXmlComments(xmlPath, true);
+public static class ConfigureSwagger {
+    public static void AddSwagger(this IServiceCollection services) {
+        services.AddSwaggerGen(options => {
+            options.SwaggerDoc("v1", new OpenApiInfo {Title = "CrawlCenter.Web API V1", Version = "v1"});
+            options.AddServer(new OpenApiServer {
+                Url = "",
+                Description = "CrawlCenter Master Server"
             });
-        }
+            options.CustomOperationIds(apiDesc => {
+                var controllerAction = apiDesc.ActionDescriptor as ControllerActionDescriptor;
+                return $"{controllerAction?.ControllerName}-{controllerAction?.ActionName}";
+            });
+
+            // 为 Swagger JSON and UI设置xml文档注释路径
+            var xmlPath = Path.Combine(System.AppContext.BaseDirectory, "CrawlCenter.Web.xml");
+            options.IncludeXmlComments(xmlPath, true);
+        });
     }
 }
--- CrawlCenter.Web/Startup.cs
+++ CrawlCenter.Web/Startup.cs
@@ -16,100 +16,100 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
-namespace CrawlCenter.Web {
-    public class Startup {
-        private readonly IConfiguration _configuration;
-        private readonly IWebHostEnvironment _env;
+namespace CrawlCenter.Web; 
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment env) {
-            _configuration = configuration;
-            _env = env;
+public class Startup {
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _env;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env) {
+        _configuration = configuration;
+        _env = env;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services) {
+        var builder = services.AddControllersWithViews();
+        if (_env.IsDevelopment()) {
+            // 添加Razor页面运行时动态编译支持
+            builder.AddRazorRuntimeCompilation();
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            var builder = services.AddControllersWithViews();
-            if (_env.IsDevelopment()) {
-                // 添加Razor页面运行时动态编译支持
-                builder.AddRazorRuntimeCompilation();
-            }
+        services.AddSettings(_configuration);
 
-            services.AddSettings(_configuration);
+        services.AddHangfire(_configuration);
+        services.AddSwagger();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-            services.AddHangfire(_configuration);
-            services.AddSwagger();
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.AddAuth(_configuration);
 
-            services.AddAuth(_configuration);
+        // 添加消息框架
+        services.AddSingleton<Messages>();
+        // 添加FreeSQL
+        services.AddFreeSql(_configuration);
+        // 添加仓储
+        services.AddRepositories();
 
-            // 添加消息框架
-            services.AddSingleton<Messages>();
-            // 添加FreeSQL
-            services.AddFreeSql(_configuration);
-            // 添加仓储
-            services.AddRepositories();
+        services.AddExceptionless(_configuration);
+    }
 
-            services.AddExceptionless(_configuration);
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        app.UseExceptionless();
+        // app.UseMiddleware<PrintRequestMiddleware>();
+
+        if (env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        }
+        else {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            app.UseExceptionless();
-            // app.UseMiddleware<PrintRequestMiddleware>();
+        app.UseStatusCodePagesWithReExecute("/Error/StatusCode/{0}");
 
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
-            else {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+        // app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-            app.UseStatusCodePagesWithReExecute("/Error/StatusCode/{0}");
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-            // app.UseHttpsRedirection();
-            app.UseStaticFiles();
+        app.UseHangfireDashboard();
 
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => {
+            c.RoutePrefix = "docs/swagger";
+            c.SwaggerEndpoint("/v1/api-docs", "V1 Docs");
+        });
+        app.UseReDoc(c => {
+            c.RoutePrefix = "docs/redoc";
+            c.SpecUrl = "/v1/api-docs";
+        });
+        app.UseKnife4UI(c => {
+            c.RoutePrefix = "docs/knife4j";
+            c.SwaggerEndpoint("/v1/api-docs", "V1 Docs");
+        });
+        app.UseRapiDocUI(c => {
+            c.RoutePrefix = "docs/rapi";
+            c.SwaggerEndpoint("/v1/api-docs", "V1 Docs");
+            c.GenericRapiConfig = new GenericRapiConfig {
+                Theme = "light",
+                AllowAuthentication = false,
+                ApiKeyLocation = "header",
+                ApiKeyName = "Authorization",
+                ApiKeyValue = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNyIiwibmFtZSI6IjQ3NjNkM2FiLTIxYzYtNGQxMi1iZDYxLTk1NTM5YjRmMDcxOCIsImp0aSI6IjRmM2UzZGZlLTkzZjYtNGRmZi04NDc1LWUxNTA0N2NiNTU4NCIsImV4cCI6MTY0MzcyMzA0NSwiaXNzIjoiZGVtb19pc3N1ZXIiLCJhdWQiOiJkZW1vX2F1ZGllbmNlIn0.4-jECsMbOSHvEhD0Xfh8NEE3uq2SVrNJEuIAHRAvhuc"
+            };
+        });
 
-            app.UseHangfireDashboard();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapSwagger("{documentName}/api-docs");
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.RoutePrefix = "docs/swagger";
-                c.SwaggerEndpoint("/v1/api-docs", "V1 Docs");
-            });
-            app.UseReDoc(c => {
-                c.RoutePrefix = "docs/redoc";
-                c.SpecUrl = "/v1/api-docs";
-            });
-            app.UseKnife4UI(c => {
-                c.RoutePrefix = "docs/knife4j";
-                c.SwaggerEndpoint("/v1/api-docs", "V1 Docs");
-            });
-            app.UseRapiDocUI(c => {
-                c.RoutePrefix = "docs/rapi";
-                c.SwaggerEndpoint("/v1/api-docs", "V1 Docs");
-                c.GenericRapiConfig = new GenericRapiConfig {
-                    Theme = "light",
-                    AllowAuthentication = false,
-                    ApiKeyLocation = "header",
-                    ApiKeyName = "Authorization",
-                    ApiKeyValue = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QtdXNyIiwibmFtZSI6IjQ3NjNkM2FiLTIxYzYtNGQxMi1iZDYxLTk1NTM5YjRmMDcxOCIsImp0aSI6IjRmM2UzZGZlLTkzZjYtNGRmZi04NDc1LWUxNTA0N2NiNTU4NCIsImV4cCI6MTY0MzcyMzA0NSwiaXNzIjoiZGVtb19pc3N1ZXIiLCJhdWQiOiJkZW1vX2F1ZGllbmNlIn0.4-jECsMbOSHvEhD0Xfh8NEE3uq2SVrNJEuIAHRAvhuc"
-                };
-            });
-
-            app.UseEndpoints(endpoints => {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapSwagger("{documentName}/api-docs");
-
-                // endpoints.MapHangfireDashboard();
-            });
-        }
+            // endpoints.MapHangfireDashboard();
+        });
     }
 }
